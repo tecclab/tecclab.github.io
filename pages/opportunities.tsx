b29c3654b96ebaf525562c0516578855aad9fc6e
Emmett Lach <emmett.lach@gmail.com>
2022-09-13 10:27:40 -0400

    Update opportunities

--- pages/opportunities.tsx
+++ pages/opportunities.tsx
@@ -17,9 +17,16 @@ const Opportunities: NextPage = () => {
           <Opportunity
             href="https://uva.wd1.myworkdayjobs.com/en-US/UVAJobs/job/Charlottesville-VA/Postdoctoral-Fellowship-in-Data-Science-and-Mobile-Health_R0037543"
             src="./favicon.ico"
-            alt="Postdoctoral Fellowship in Data Science and Mobile Health"
+            alt="Postdoctoral Fellowship in Cancer Health Disparities and Technology"
           >
             Postdoctoral Fellowship in Cancer Health Disparities and Technology
+          </Opportunity>
+          <Opportunity
+            href="https://uva.wd1.myworkdayjobs.com/en-US/UVAJobs/details/Postdoctoral-Fellowship-in-Compassionate-Care--Compassionate-Care-Postdoctoral-Research-Associate-_R0040320"
+            src="./favicon.ico"
+            alt="Postdoctoral Fellowship in Compassionate Care"
+          >
+            Postdoctoral Fellowship in Compassionate Care
           </Opportunity>
           <Opportunity
             href="https://docs.google.com/document/d/1V9XJXWULqSbCyCVM3UTW-OjMbIKuClLV/edit?usp=sharing&ouid=114197689007378729669&rtpof=true&sd=true"
